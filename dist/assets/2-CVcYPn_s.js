d44142b409a443be3d57fcdae5382ecc34626e71
will.ai <airbknows@gmail.com>
2025-05-05 14:36:11 +0800

    gitignore updated

--- dist/assets/2-CVcYPn_s.js
+++
@@ -1,1 +0,0 @@
-import{bc as e,a7 as c,$ as n}from"./index-D7x1RRRi.js";const t={};function a(o,r){return n(),c("div",null," Navigation 2 ")}const _=e(t,[["render",a]]);export{_ as default};
--- dist/assets/item-1-BZJRzWBh.js
+++
@@ -1,1 +0,0 @@
-import{bc as e,a7 as c,$ as t}from"./index-D7x1RRRi.js";const n={};function r(o,a){return t(),c("div",null," Item One ")}const _=e(n,[["render",r]]);export{_ as default};
